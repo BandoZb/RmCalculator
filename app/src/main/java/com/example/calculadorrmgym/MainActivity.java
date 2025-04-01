fd7d1ccae64aa56a954f4dd5747c24aace4a644e
BandoZb <fvp0004@alu.medac.es>
2025-04-01 09:21:38 +0200

    Agregacion numeros decimales

--- app/src/main/java/com/example/calculadorrmgym/MainActivity.java
+++ app/src/main/java/com/example/calculadorrmgym/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.calculadorrmgym;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etKg, etRepes, etResultado;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Vinculación con los elementos del XML
+        etKg = findViewById(R.id.etKg);
+        etRepes = findViewById(R.id.etRepes);
+        etResultado = findViewById(R.id.etResultado);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        // Evento de clic para el botón
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String etNumeroKilos = etKg.getText().toString();
+                String etNumeroRepes = etRepes.getText().toString();
+
+                // Verificar que los campos no estén vacíos
+                if (etNumeroKilos.isEmpty() || etNumeroRepes.isEmpty()) {
+                    etResultado.setText("Ingrese un número");
+                    return; // Salir del método para evitar errores
+                }
+
+                try {
+                    double kilos = Double.parseDouble(etNumeroKilos);
+                    double repes = Double.parseDouble(etNumeroRepes);
+
+                    // Cálculo del RM
+                    double calculoRm = (kilos * 0.033 * repes) + kilos -1 ;
+
+                    // Mostrar el resultado correctamente
+                    etResultado.setText(String.format("%.1f", calculoRm));
+                } catch (NumberFormatException e) {
+                    etResultado.setText("Escribe bien tontito");
+                }
+            }
         });
     }
 }
